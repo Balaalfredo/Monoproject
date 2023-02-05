6f168634950a1be64b7f13075fa9f4c2961a0eb0
Balaalfredo <balas6664@gmail.com>
2023-02-05 20:08:31 +0530

    'shadow'

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const bar = document.querySelector('.bar');
 const downdiv = document.querySelector('.downdiv');
+const navbar = document.querySelector('.navbar');
 console.log(downdiv);
 bar.addEventListener('click', () => {
     if (downdiv.classList.contains('selectdown')) {
         downdiv.classList.remove('selectdown');
     }
     else { downdiv.classList.add('selectdown') }
+})
+
+window.addEventListener('scroll', () => {
+
+    const scrollvalue = window.scrollY;
+    console.log(scrollvalue)
+    if (scrollvalue >= 100) {
+        navbar.style.boxShadow = '2px 2px 1px 1px  #009DFF,4px 2px 1px 1px  #009DFF';
+    }
+    else { navbar.style.boxShadow = '0px 0px 0px 0px'; }
 })
